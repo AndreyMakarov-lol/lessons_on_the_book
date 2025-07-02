e284e04a25876d960387c3be0872426a54c0ef83
Andrey_Makarov <64976956+AndreyMakarov-lol@users.noreply.github.com>
2025-07-02 20:57:20 +0300

    8.1-8.2

---
+++ upr/func.py
@@ -0,0 +1,10 @@
+#8.1
+# def display_message():
+#     print('Это глава про функции')
+# display_message()
+
+# # #8.2
+# def favorite_book(title):
+#     print(f'Моя любимая книга- {title.capitalize()}')
+# title_book=input('Введи название книги: ')
+# favorite_book(title_book)
