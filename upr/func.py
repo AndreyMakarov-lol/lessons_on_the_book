587473a76c64b09197b5067d5f8bf0a030089889
Andrey_Makarov <64976956+AndreyMakarov-lol@users.noreply.github.com>
2025-07-03 17:33:24 +0300

    8.3-8.4

--- upr/func.py
+++ upr/func.py
@@ -8,3 +8,13 @@
 #     print(f'Моя любимая книга- {title.capitalize()}')
 # title_book=input('Введи название книги: ')
 # favorite_book(title_book)
+
+
+#8.3-8.4
+# def make_shirt(size ='L', text = 'I love Hyi'):
+#     shirt = f"It`s shirt {size}, and have next text: '{text}'"
+#     return shirt
+# new_shirt = make_shirt(25, 'Ты хуй')
+# print(new_shirt)
+# last_shirt = make_shirt()
+# print(last_shirt)
