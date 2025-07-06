483628b6103967f259a9de6ce322494212feb7ad
Andrey_Makarov <64976956+AndreyMakarov-lol@users.noreply.github.com>
2025-07-06 14:23:40 +0300

    8.9-8.11

--- upr/func.py
+++ upr/func.py
@@ -28,3 +28,68 @@
 # discribe_city('Питер')
 # discribe_city('Барселона', 'Испания')
 # discribe_city('Париж', 'Франция')
+
+#8.6
+# def city_country(city, country):
+#     otvet = f'{city.title()}, {country.title()}'
+#     return otvet
+# def_otvet = city_country('москва','россия')
+# print(def_otvet)
+
+# #8.7-8.8
+#
+# def make_album(name, info, lines='' ):
+#     album = {
+#         'name': name,
+#         'album': info,
+#         **({'lines': lines} if lines else {})
+#     }
+#     return album
+#
+#
+# while True:
+#     artist_name = input('Pleas write artist name: ')
+#     album_name = input('Pleas write album name: ')
+#     lines = input('Pleas write lines nums or no: ')
+#     if lines == 'no':
+#         new_album = make_album(artist_name, album_name)
+#         print(new_album)
+#         break
+#     else:
+#         new_album = make_album(artist_name, album_name, lines)
+#         print(new_album)
+#         break
+
+#8.9
+# messages_text = ['Hui na', 'zyi dva', 'coci tri']
+# def show_messages(messages):
+#     for message in messages:
+#         print(f'{message}')
+#
+# show_messages(messages_text)
+# show_messages(messages_text)
+#8.10
+# messages_text = ['Hui na', 'zyi dva', 'coci tri']
+# send_messages = []
+# def show_messages(messages):
+#     while messages:
+#         message = messages.pop()
+#         print(f'Send {message}')
+#         send_messages.append(message)
+#
+# show_messages(messages_text)
+# show_messages(messages_text)
+# print(send_messages)
+
+#8.11
+#
+# messages_text = ['Hui na', 'zyi dva', 'coci tri']
+# send_messages = []
+# def show_messages(messages):
+#     while messages:
+#         message = messages.pop()
+#         print(f'Send {message}')
+#         send_messages.append(message)
+# show_messages(messages_text[:])
+# print(send_messages)
+# print(messages_text)
